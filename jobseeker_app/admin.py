006508cbc88a2605143741fea84df5b968c392df
Colton Windsor <Coltonwindsor@yahoo.com>
2020-02-04 14:45:36 -0500

    registered models

--- jobseeker_app/admin.py
+++ jobseeker_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Document, Event, Job, Response, Contact
 # Register your models here.
+
+admin.site.register([Document, Event, Job, Response, Contact])
