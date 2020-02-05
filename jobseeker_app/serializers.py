1ceb0238df88660e1c8f0e86b91f6e57db409872
Colton Windsor <Coltonwindsor@yahoo.com>
2020-02-04 19:48:07 -0500

    set up serializers and views

---
+++ jobseeker_app/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from .models import Document, Event, Job, Response, Contact
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Document
+        fields = ('__all__')
+
+class ResponseSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Document
+        fields = ('__all__')
+
+class JobSerializer(serializers.ModelSerializer):
+    respone = ResponseSerializer(many=True, read_only=True)
+    class Meta: 
+        model = Job
+        fields = ('id', 'company', 'title', 'date_posted', 'description', 'requirements', 'salary', 'applied_to', 'response' )
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Event
+        fields = ('__all__')
+
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Document
+        fields = ('__all__')
+
+
+
--- jobseeker_app/views.py
+++ jobseeker_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Document, Event, Job, Response, Contact
+from .serializers import DocumentSerializer, EventSerializer, JobSerializer, ResponseSerializer, ContactSerializer
 
-# Create your views here.
+class DocumentView(viewsets.ModelViewSet):
+    queryset = Document.objects.all()
+    serializer_class = DocumentSerializer
+
+class EventView(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+class JobView(viewsets.ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+class ResponseView(viewsets.ModelViewSet):
+    queryset = Response.objects.all()
+    serializer_class = ResponseSerializer
+
+class ContactView(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
