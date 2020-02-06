6f537773650a380d486b788105d051b24127e2e9
Colton Windsor <Coltonwindsor@yahoo.com>
2020-02-06 14:20:02 -0500

    single contact page working

--- client/src/components/SingleContact.js
+++ client/src/components/SingleContact.js
@@ -1,10 +1,75 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
 export default class SingleContact extends Component {
+    state = {
+        contact: {
+            name: '',
+            company: '',
+            email: '',
+            phone_number: ''
+        },
+        redirect: false,
+        updateFormInvisable: false
+    }
+    componentDidMount() {
+        axios.get(`/api/v1/contact/${this.props.match.params.contactId}/`)
+            .then((res) => {
+                console.log(res.data)
+                this.setState({ contact: res.data })
+            })
+    }
+    onChange = (evt) => {
+        const value = evt.target.value
+        const name = evt.target.name
+        const copyOfState = { ...this.state }
+        copyOfState.contact[name] = value
+        this.setState(copyOfState)
+    }
+    onSubmit = (evt) => {
+        evt.preventDefault()
+        axios.put(`/api/v1/contact/${this.props.match.params.contactId}/`, this.state.contact)
+            .then(() => {
+                this.setState({ redirect: true })
+            })
+    }
+    toggleUpdateForm = () => {
+        const toggle = !this.state.updateFormInvisable
+        this.setState({ updateFormInvisable: toggle })
+    }
+    deleteContact = () => {
+        axios.delete(`/api/v1/contact/${this.props.match.params.contactId}/`)
+            .then(() => {
+                this.setState({ redirect: true })
+            })
+    }
+
     render() {
         return (
             <div>
-                <h1>Hello from single contact page</h1>
+                {this.state.redirect === true ? <Redirect to='/contact' /> : null}
+                <h1>{this.state.contact.name}</h1>
+                <div>
+                    <div>
+                        {this.state.contact.company}
+                    </div>
+                    <div>
+                        {this.state.contact.email}
+                    </div>
+                    <div>
+                        {this.state.contact.phone_number}
+                    </div>
+                </div>
+                {this.state.updateFormInvisable === false ?
+                    <div className="addEntryButtonDiv">
+                        <button className="addEntryButton" onClick={this.toggleUpdateForm}>Edit Contact</button>
+                        <button className="addEntryButton" onClick={this.deleteContact}>Delete Contact</button>
+                    </div> :
+                    <div className="addEntryButtonDiv">
+                        <button className="addEntryButton" onClick={this.toggleUpdateForm}>Back</button>
+                        <button className="addEntryButton" onClick={this.deleteContact}>Delete Contact</button>
+                    </div>}
             </div>
         )
     }
