852f9abee0ef7b00d4fea80c21794f10bb99f220
Colton Windsor <Coltonwindsor@yahoo.com>
2020-02-06 14:54:42 -0500

    documents page shows current docs

--- client/src/components/Document.js
+++ client/src/components/Document.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 
 export default class Document extends Component {
+    state = {
+        documents: [],
+        document: {
+            resume: '',
+            cover_letter: '',
+            linkedin_link: '',
+            github_link: '',
+            portfolio_link: ''
+        },
+        addDocumentInvisable: false
+    }
+    componentDidMount() {
+        this.reloadDocumentsPage()
+    }
+    reloadDocumentsPage = () => {
+        axios.get('/api/v1/document/')
+            .then((res) => {
+                this.setState({ documents: res.data })
+            })
+    }
     render() {
+        const allDocuments = this.state.documents.map((document) => {
+            return (
+                <div>
+                    <div>{document.resume}</div>
+                    <div>{document.cover_letter}</div>
+                    <div>{document.linkedin_link}</div>
+                    <div>{document.github_link}</div>
+                    <div>{document.portfolio_link}</div>
+                </div>
+            )
+        })
         return (
             <div>
-                <h1>Hello from documents page</h1>
+                <h1>Important Documents</h1>
+                {allDocuments}
             </div>
         )
     }
